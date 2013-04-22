55cd2467d455a84c746c6bd8b8c8e7850f05c348
Dan Shearmur <dan.shearmur@semantico.com>
2013-04-22 17:15:49 +0100

    Added empty gif

--- routes/logger.js
+++ routes/logger.js
@@ -1,14 +1,28 @@
-var url  = require('url');
+var url = require('url');
+var emptygif = require('emptygif');
 
 function log(string, ua, ip) {
     console.log(string, ua, ip);
 }
 
 exports.post = function (req, res) {
-    log(req.body._q, req.headers['user-agent'], req.connection.remoteAddress);
+    process.nextTick(function () {
+        log(req.body._q, req.headers['user-agent'], req.connection.remoteAddress);
+    });
+    res.setHeader('Cache-Control', 'public, max-age=0');
+    return res.send('');
 };
 
 exports.get = function (req, res) {
     var query = url.parse(req.url, true).query;
-    log(query._q, req.headers['user-agent'], req.connection.remoteAddress);
+    process.nextTick(function () {
+        log(query._q, req.headers['user-agent'], req.connection.remoteAddress);
+    });
+
+    emptygif.sendEmptyGif(req, res, {
+        'Content-Type' : 'image/gif',
+        'Content-Length' : emptygif.emptyGifBufferLength,
+        'Cache-Control' : 'public, max-age=0'
+    });
+
 };
