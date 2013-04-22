a3a0a3800220270933c5b5149fae522af4fd1a33
Dan Shearmur <dan.shearmur@semantico.com>
2013-04-22 17:35:43 +0100

    Removed unused stuff

--- app.js
+++ app.js
@@ -13,14 +13,11 @@ var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.cookieParser());
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
