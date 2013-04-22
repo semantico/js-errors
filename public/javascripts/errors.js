a4f2ce2b4f76cc1fc3f6e08590cccfc6c42d4111
Dan Shearmur <dan.shearmur@semantico.com>
2013-04-22 17:32:32 +0100

    Fleshing out errors.js

--- public/javascripts/errors.js
+++ public/javascripts/errors.js
@@ -1,8 +1,61 @@
-var o = {
-    some: 'thing',
-    more: ['things', 'in', 'here']
-};
+// JSON shim
+// document ready shim - https://github.com/ded/domready/blob/master/src/ready.js
 
-function tmp(o, key) {
-    return o[key] !== undefined;
+function guid() {
+
 }
+
+function getId() {
+
+}
+
+function setId() {
+
+}
+
+function hasLocalStorage() {
+    try {
+        return ('localStorage' in window && window['localStorage'] !== null);
+    } catch (e) {
+        return false;
+    }
+}
+
+function parseModernizr() {
+
+}
+
+function isValid(msg) {
+
+}
+
+function rateLimit() {
+
+}
+
+function isLimited() {
+
+}
+
+function logError(msg, url, line) {
+
+}
+
+function get() {
+
+}
+
+function post() {
+
+}
+
+function send() {
+
+}
+
+function load() {
+    // check localStorage and send if they are any
+}
+
+domReady(load);
+
