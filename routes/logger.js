94596fdfd15c82c3224b274ddfbcb744d018abb0
Dan Shearmur <dan.shearmur@semantico.com>
2013-04-22 23:07:38 +0100

    fix query param and pull out details

--- routes/logger.js
+++ routes/logger.js
@@ -2,12 +2,17 @@ var url = require('url');
 var emptygif = require('emptygif');
 
 function log(string, referer, ua, ip) {
-    console.log(string, referer, ua, ip);
+    var msg = JSON.parse(string);
+    var guid = msg[0];
+    var errors = msg[1];
+    var can = msg[2][0];
+    var cant = msg[2][0];
+    console.log(guid, errors, referer, ua, ip, can, cant);
 }
 
 exports.post = function (req, res) {
     process.nextTick(function () {
-        log(req.body._q, req.headers['referer'], req.headers['user-agent'], req.connection.remoteAddress);
+        log(req.body.q, req.headers['referer'], req.headers['user-agent'], req.connection.remoteAddress);
     });
     res.setHeader('Cache-Control', 'public, max-age=0');
     return res.send('');
@@ -16,7 +21,7 @@ exports.post = function (req, res) {
 exports.get = function (req, res) {
     var query = url.parse(req.url, true).query;
     process.nextTick(function () {
-        log(query._q, req.headers['referer'], req.headers['user-agent'], req.connection.remoteAddress);
+        log(query.q, req.headers['referer'], req.headers['user-agent'], req.connection.remoteAddress);
     });
 
     emptygif.sendEmptyGif(req, res, {
