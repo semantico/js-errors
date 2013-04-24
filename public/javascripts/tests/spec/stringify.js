bf1907380f48a2117965cea2dd81ae8be66e2ff6
Dan Shearmur <dan.shearmur@semantico.com>
2013-04-24 12:32:40 +0100

    Refactored stringify tests

--- public/javascripts/tests/spec/stringify.js
+++ public/javascripts/tests/spec/stringify.js
@@ -2,45 +2,59 @@ define(['stringify'], function (stringify) {
 
     describe('stringify', function () {
 
+        var mixedArray;
+        var mixedArrayJSON;
+
+        beforeEach(function () {
+            mixedArray = [0, 1, 2, ['abc', 'xyz'], 'test'];
+            mixedArrayJSON = JSON.stringify(mixedArray);
+        });
+
         it('should be a function', function () {
 
             stringify.should.be.a('function');
 
         });
 
-        if (typeof JSON !== "undefined" && JSON !== null) {
+        it('should handle a simple array of numbers', function (){
+            var array = [0, 1, 2, 3];
+            JSON.stringify(array).should.equal(stringify(array));
+        });
 
-            it('should handle a simple array of numbers', function (){
-                var array = [0, 1, 2, 3];
-                JSON.stringify(array).should.equal(stringify(array));
-            });
+        it('should handle two dimensional arrays of numbers', function (){
+            var array = [[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]];
+            JSON.stringify(array).should.equal(stringify(array));
+        });
 
-            it('should handle two dimensional arrays of numbers', function (){
-                var array = [[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]];
-                JSON.stringify(array).should.equal(stringify(array));
-            });
+        it('should handle three dimensional arrays of numbers', function (){
+            var array = [[[0, 1, 2, 3], [0, 1, 2, 3]], [[0, 1, 2, 3], [0, 1, 2, 3]]];
+            JSON.stringify(array).should.equal(stringify(array));
+        });
 
-            it('should handle three dimensional arrays of numbers', function (){
-                var array = [[[0, 1, 2, 3], [0, 1, 2, 3]], [[0, 1, 2, 3], [0, 1, 2, 3]]];
-                JSON.stringify(array).should.equal(stringify(array));
-            });
+        it('should handle a simple array of strings', function (){
+            var array = ['0', '1', '2', '3'];
+            JSON.stringify(array).should.equal(stringify(array));
+        });
 
-            it('should handle a simple array of strings', function (){
-                var array = ['0', '1', '2', '3'];
-                JSON.stringify(array).should.equal(stringify(array));
-            });
+        it('should handle two dimensional arrays of strings', function (){
+            var array = [['0', '1', '2', '3'], ['0', '1', '2', '3'], ['0', '1', '2', '3']];
+            JSON.stringify(array).should.equal(stringify(array));
+        });
 
-            it('should handle two dimensional arrays of strings', function (){
-                var array = [['0', '1', '2', '3'], ['0', '1', '2', '3'], ['0', '1', '2', '3']];
-                JSON.stringify(array).should.equal(stringify(array));
-            });
+        it('should handle three dimensional arrays of strings', function (){
+            var array = [[['0', '1', '2', '3'], ['0', '1', '2', '3']], [['0', '1', '2', '3'], ['0', '1', '2', '3']]];
+            JSON.stringify(array).should.equal(stringify(array));
+        });
 
-            it('should handle three dimensional arrays of strings', function (){
-                var array = [[['0', '1', '2', '3'], ['0', '1', '2', '3']], [['0', '1', '2', '3'], ['0', '1', '2', '3']]];
-                JSON.stringify(array).should.equal(stringify(array));
-            });
+        it('should handle a mixed array', function (){
+            JSON.stringify(mixedArray).should.equal(stringify(mixedArray));
+        });
 
-        }
+        it('should work when mapped to JSON.stringify', function (){
+            var JSON = {};
+            JSON.stringify = stringify;
+            JSON.stringify(mixedArray).should.equal(mixedArrayJSON);
+        });
 
     });
 
