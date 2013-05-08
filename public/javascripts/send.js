f074331257da0337bc92e8eb2573d9001ded543a
Dan Shearmur <dan.shearmur@semantico.com>
2013-05-08 11:36:20 +0100

    Revert "Updated send to require data-errorurl attribute on script tag"
    This reverts commit 66dbb4ef77e6f6d747f952a70430b4e0f6b5b7c4.

--- public/javascripts/send.js
+++ public/javascripts/send.js
@@ -1,56 +1,26 @@
 define(function () {
 
+    var URL = 'http://localhost:3000/';
     var PARAM = 'q';
     var REFERER_PARAM = 'r';
     var TIMEOUT = 5000;
     var IFRAME_ID = 'err-send__';
 
-    function warn() {
-        if (window.console) {
-            console.log('No error url');
-        }
-    }
-
-    var url;
-
-    function getUrl() {
-        var s = 'script';
-        var e = 'data-errorurl';
-        if (document.querySelectorAll) {
-            return document.querySelector(s + '[' + e + ']').getAttribute(e);
-        }
-        var scripts = document.getElementsByTagName(s);
-        for (var i = 0, l = scripts.length; i < l; i++) {
-            var data = scripts[i].getAttribute(e);
-            if (data) {
-                return data;
-            }
-        }
-    }
-
     function get(json) {
-        url = url || getUrl();
-        if (!url) {
-            return warn();
-        }
         var image = new Image();
-        image.src = url + '?' + PARAM + '=' + encodeURI(json);
+        image.src = URL + '?' + PARAM + '=' + encodeURI(json);
     }
 
     var body;
     var iframeCount = 0;
 
     function post(json) {
-        url = url || getUrl();
-        if (!url) {
-            return warn();
-        }
         body = body || document.getElementsByTagName('body')[0];
         var iframe = document.createElement('iframe');
         iframe.id = iframe.name = IFRAME_ID + iframeCount;
         iframe.style.display = 'none';
         iframe.src = 'javascript:';
-        var html = '<form method="post" action="' + url + '">' +
+        var html = '<form method="post" action="' + URL + '">' +
             '<textarea name="' + REFERER_PARAM + '">' + location.href + '</textarea>' +
             '<textarea name="' + PARAM + '">' + json + '</textarea>' +
             '</form>' +
